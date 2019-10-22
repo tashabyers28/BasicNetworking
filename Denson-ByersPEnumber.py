d9daf3f9b5f2519836e810bf16fb9cc0f924dcca
tashabyers28 <56890015+tashabyers28@users.noreply.github.com>
2019-10-22 13:13:38 -0500

    Add files via upload

---
+++ Denson-ByersPEnumber.py
@@ -0,0 +1,19 @@
+#V is the number of grapevines that will fit in the row.
+#R is the length of rows, in feet.
+#E is the amount of space, in feet, used by an end-post assembly.
+#S is the space between vines, in feet.
+
+#Ask what the length of the row, in feet
+R = float(input('Enter the length of the row in feet: '))
+
+#Ask what is the amount of space used by an end-post assembly, in feet
+E = float(input('Enter the amount of space used by an end-post assembly in feet: '))
+
+#Ask what is the amount of space between the vines, in feet
+S = float(input('Enter the amount of space between vines in feet: '))
+
+#Once the input data has been entered, the program should calculate and display the number of grapevines that will fit in the row.
+
+V = (R - 2*E)/S
+
+Print('This is the number of grapevines that will fit into the row:', V)
